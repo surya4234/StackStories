d49256abfdfce6cf2cb03bff89e1a254e3d631d3
surya4234 <165979309+surya4234@users.noreply.github.com>
2026-04-17 14:59:11 +0530

    Update API base URL for axios instance

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://stack-stories-1wpltik9l-suryas-projects-521dbc15.vercel.app",
+  baseURL: "https://stack-stories-9m8cydcxa-suryas-projects-521dbc15.vercel.app",
 });
 
 // Attach token
